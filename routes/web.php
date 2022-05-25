b6c79a0c88a89f35b5c16b89f9ce2329044bdff5
Israelssj <98783537+Israelssj@users.noreply.github.com>
2022-05-25 15:23:25 -0500

    datos

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DuenioController;
 use App\Http\Controllers\MascotaController;
 use App\Http\Controllers\VeterinariaController;
-<<<<<<< HEAD
 
 
-=======
+
+
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\ForodudasController;
 use Illuminate\Support\Facades\Auth;
->>>>>>> b25a5a9573a74bc73c78dcde2f85d845d17bcbb3
+
 
 Route::resources([
     'veterinaria'   => VeterinariaController::class,
@@ -28,16 +28,5 @@ Route::resources([
     'forodudas'   => ForodudasController::class,
 ]);
 
-<<<<<<< HEAD
-    Route::resource(
-        'mascota', MascotaController::class);
-
-Route::get('Veterinaria/datatable', [ComputadoraController::class,'datatable']);
-Route::get('/',function() {
-=======
-Route::get('/', function () {
->>>>>>> b25a5a9573a74bc73c78dcde2f85d845d17bcbb3
-    return view('welcome');
-});
 
 
